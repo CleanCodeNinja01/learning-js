d2ae3a26b59de59f4c5d96b29d5d82f33a1cdbdf
Saleha Shujaat <saleha.shujaat@bazaartech.com>
2022-10-25 07:11:52 +0500

    Problem: best time to buy and sell stock with txn fee

---
+++ Problems/buy-and-sell-stock.js
@@ -0,0 +1,40 @@
+/* you are given an array "prices" where "prices[i]"" is the price of the given stock item on the ith day and an integer representing a transaction fee
+
+Find the maximum profit you can achieve. You may complete as many transactions as you like, but you need to pay the transaction fee for each transaction.
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again). 
+
+Example 1:
+Input: prices = [1,3,2,8,4,9], fee = 2
+Output: 8
+
+Example 2:
+Input: prices = [1,3,7,5,10,3], fee = 3
+Output: 6
+
+*/ 
+
+let maxStocksProfit = function(prices, fee) {
+    let prevProfit, currentProfit, maxProfit = 0;
+    let prevEffectBuying, currentEffectiveBuying, minEffectiveBuying = 0;
+    let i = 0;
+    while (i<prices.length) {
+        if (i==0){
+            prevProfit = 0;
+            prevEffectBuying = prices[i];
+        }
+        else{
+            currentProfit = prices[i] - minEffectiveBuying - fee;
+            maxProfit = Math.max(prevProfit, currentProfit);
+            currentEffectiveBuying = prices[i] - maxProfit;
+            minEffectiveBuying = Math.min(prevEffectBuying, currentEffectiveBuying);
+            prevProfit = maxProfit;
+            prevEffectBuying = minEffectiveBuying;
+        }
+        i++;
+    }
+    return maxProfit;
+}
+
+let prices = [1,3,7,5,10,3], fee = 3
+console.log(maxStocksProfit(prices, fee));
