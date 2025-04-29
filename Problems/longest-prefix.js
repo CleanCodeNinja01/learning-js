0538cfc16b702d67ac98790c78d63581a2024678
Saleha Shujaat <salehashujaat@salehas-mbp.home>
2025-04-29 13:53:19 +0500

    new way for longestprefix

--- Problems/longest-prefix.js
+++ Problems/longest-prefix.js
@@ -6,10 +6,6 @@ var longestCommonPrefix = function(strs) {
     for(let i=1; i<strs.length; i++){
         // while ocndition jab tak 0 k barabar nai hojati this will run, and 0 k barabar sirf tab hogi jab prefix match karega
         while(strs[i].indexOf(prefix) !== 0){
-            console.log('ceh',strs[i])
-            console.log('b', strs[i].indexOf(prefix))
-
-            console.log({prefix})
             prefix = prefix.substring(0, prefix.length-1)
             console.log({prefix})
             if (prefix==""){
@@ -20,5 +16,21 @@ var longestCommonPrefix = function(strs) {
     return prefix
 };
 
-const strs = ["flower", "flowht", "flowht1"];
-console.log(longestCommonPrefix(strs)); // Output: "fl"
+const strs = ["flower", "flaunt", "flow", "flight"];
+
+
+var longestCommonPrefix2 = function(strs){
+    let prefix = strs[0];
+
+    for (let i=0; i<strs.length; i++){
+        while(!strs[i].startsWith(prefix)){
+            prefix = prefix.slice(0, -1);
+            if (prefix == ""){
+                return ""
+            }
+        }
+    }
+    return prefix;
+}
+
+console.log(longestCommonPrefix2(strs)); // Output: "fl"
